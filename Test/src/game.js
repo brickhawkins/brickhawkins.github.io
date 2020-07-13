61d4da1563ecdb5d883d36a029c4aa60aa47221f
brickhawkins <brickhawkins@gmail.com>
2020-07-13 20:55:26 +0700

    Add files via upload

--- Test/src/game.js
+++ Test/src/game.js
@@ -1,16 +1,18 @@
 var game;
 var dpr = window.devicePixelRatio;
-const width = 600 * dpr;
-const height = 600 * dpr;
 
 window.onload = function() {
 
-    console.log(window.devicePixelRatio);
+    console.log("device pixel ratio:" + window.devicePixelRatio);
 
     var gameConfig = {
        type: Phaser.CANVAS,
-       width: width,
-       height: height,
+       scale: {
+        mode: Phaser.Scale.FIT, // we will resize the game with our own code (see Boot.js)
+        width: 600 * window.devicePixelRatio, // set game width by multiplying window width with devicePixelRatio
+        height: 600 * window.devicePixelRatio, // set game height by multiplying window height with devicePixelRatio
+        zoom: 1 / window.devicePixelRatio // Set the zoom to the inverse of the devicePixelRatio
+        },
        backgroundColor: 0x880044,
        audio: {
         disableWebAudio: true
@@ -21,8 +23,8 @@ window.onload = function() {
 
         // pure javascript to give focus to the page/frame and scale the game
         window.focus();
-        resize();
-        window.addEventListener("resize", resize, false);   
+        //resize();
+        //window.addEventListener("resize", resize, false);   
 }
 
 // PlayGame scene
@@ -35,15 +37,25 @@ class playGame extends Phaser.Scene{
 
     // method to be executed when the scene preloads
     preload(){
-        this.load.image("wheel", "assets/wheel_bg.png");
-        
+        if(dpr == 3){
+            this.load.image("wheel", "assets/wheel@3.png");
+            this.load.image("background", "assets/bg@3.png");
+        }else if (dpr == 2){
+            this.load.image("wheel", "assets/wheel@2.png");
+            this.load.image("background", "assets/bg@2.png");
+        }else{
+            this.load.image("wheel", "assets/wheel@1.png");
+            this.load.image("background", "assets/bg@1.png");
+        }
+
         this.halfWidth = game.config.width / 2;
         this.halfHeight = game.config.height / 2;
     }
 
     // method to be executed once the scene has been created
     create(){
-        this.wheel = this.add.sprite(this.halfWidth, this.halfHeight, "wheel").setInteractive();
+        this.bg = this.add.sprite(this.halfWidth, this.halfHeight, "background");
+        this.wheel = this.add.sprite(this.halfWidth, this.halfHeight + (20 * dpr), "wheel").setInteractive();
 
         this.wheel.on("pointerdown", this.spin, this);
     }
@@ -77,8 +89,8 @@ function resize() {
     var canvas = document.querySelector("canvas");
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-    var windowRatio = windowWidth / windowHeight;
-    var gameRatio = game.config.width / game.config.height;
+    var windowRatio = (windowWidth / windowHeight );
+    var gameRatio = (game.config.width / game.config.height) ;
 
     if(windowRatio < gameRatio){
         canvas.style.width = windowWidth + "px";
